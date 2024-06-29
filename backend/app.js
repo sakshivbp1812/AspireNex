8ef57f1e25b977f798e83187b1d4bedb27914071
Paras Raut <parasraut2511@gmail.com>
2024-06-29 15:53:13 +0530

    s

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,9 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow requests from any origin
+}));
 // Body parser middleware
 app.use(express.json());
 app.use('/api/quizzes', quizRoutes);
